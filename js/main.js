da9aea1745ba0c4f4aaac1b19b065e4eb7599294
ReenExe <ipourri@gmail.com>
2017-11-20 20:14:44 +0200

    standard state #3

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     const towerState = {
-        1: [4, 5, 6, 7],
-        2: [1],
-        3: [2],
-        4: [3]
+        1: [1, 2, 3],
+        2: [],
+        3: [],
+        4: []
     };
 
     renderTowerState(towerState);
