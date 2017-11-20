435f92a5c1ef88a61686ad447f587b2c9253dfcd
ReenExe <ipourri@gmail.com>
2017-11-20 20:13:58 +0200

    extract function #3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 $(document).ready(function () {
-    function getDisk(index) {
-        return `<li class="disk disk-${index}"></li>`;
-    }
-
     const towerState = {
         1: [4, 5, 6, 7],
         2: [1],
@@ -10,19 +6,7 @@ $(document).ready(function () {
         4: [3]
     };
 
-    for (let towerIndex in towerState) {
-        if (towerState.hasOwnProperty(towerIndex)) {
-            const $tower = $(`#tower-${towerIndex}`);
-
-            const disks = towerState[towerIndex];
-
-            for (let diskIndex = 0; diskIndex < disks.length; ++diskIndex) {
-                const disk = disks[diskIndex];
-
-                $tower.prepend(getDisk(disk));
-            }
-        }
-    }
+    renderTowerState(towerState);
 });
 
 function hanoi(n, from, to, via) {
@@ -33,4 +17,26 @@ function hanoi(n, from, to, via) {
     moveDisk(from, to);
 
     hanoi(n - 1, via, to, from);
+}
+
+function renderTowerState(towerState) {
+    for (let towerIndex in towerState) {
+        if (towerState.hasOwnProperty(towerIndex)) {
+            const $tower = $(`#tower-${towerIndex}`);
+
+            $tower.html('');
+
+            const disks = towerState[towerIndex];
+
+            for (let diskIndex = 0; diskIndex < disks.length; ++diskIndex) {
+                const disk = disks[diskIndex];
+
+                $tower.prepend(getDisk(disk));
+            }
+        }
+    }
+}
+
+function getDisk(index) {
+    return `<li class="disk disk-${index}"></li>`;
 }
