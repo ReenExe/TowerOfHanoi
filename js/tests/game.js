eac5f8aad6893505cfd0ed6c6208146c9bfc9ddb
ReenExe <ipourri@gmail.com>
2017-11-22 01:54:51 +0200

    cover change state #4

--- js/tests/game.js
+++ js/tests/game.js
@@ -1,9 +1,15 @@
-QUnit.test( 'Test canMoveBetween', function( assert ) {
+QUnit.test( 'From `begin state` canMoveBetween & move & asset state', function( assert ) {
     const towerState = TowerState.getDefault();
     const moveHitsory = new MoveDiskHistory();
 
     const game = new Game(towerState, moveHitsory);
 
     assert.ok(game.canMoveBetween(TOWER.START, TOWER.RESULT));
+    game.move(TOWER.START, TOWER.RESULT);
     assert.ok(game.canMoveBetween(TOWER.START, TOWER.VIA));
+    game.move(TOWER.START, TOWER.VIA);
+
+    assert.deepEqual(towerState[TOWER.START].disks, [3]);
+    assert.deepEqual(towerState[TOWER.VIA].disks, [2]);
+    assert.deepEqual(towerState[TOWER.RESULT].disks, [1]);
 });
